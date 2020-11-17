f0a36f980688d15767c3af38cfad0be6f9d2111c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-17 23:22:27 +0100

    add price checking test

--- backend/ceneo_main/main.go
+++ backend/ceneo_main/main.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
-	m := ceneo.SearchForItem("xiaomi redmi note 8t")
+	// test searching
+	m := ceneo.SearchForItem("xiaomi redmi note 8t", true)
 	for name, url := range m {
 		fmt.Println(name + "|" + url)
 	}
+
+	// test price checking
+	price := ceneo.CheckPrice("https://www.ceneo.pl/87825152")
+	fmt.Println(price)
 }
