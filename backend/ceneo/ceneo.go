817dbc6492cb37cb91e51eda1e1ff2cf96e7be4f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-17 20:48:40 +0100

    basic configuration of collector

--- backend/ceneo/ceneo.go
+++ backend/ceneo/ceneo.go
@@ -1,1 +1,42 @@
 package ceneo
+
+import (
+	"time"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func searchForItem(name string) map[string]string {
+	results := make(map[string]string)
+	url := "https://www.ceneo.pl/;szukaj-" + name
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.ceneo.pl"),
+	)
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "www.ceneo.pl/*",
+		Delay:       10 * time.Second,
+		RandomDelay: 10 * time.Second,
+	})
+
+	c.OnHTML("a.js_pagination-top-next", func(h *colly.HTMLElement) {
+		link := h.Request.AbsoluteURL(h.Attr("href"))
+	})
+
+	c.OnHTML("a.grid-item__thumb[href]", func(h *colly.HTMLElement) {
+		if h.DOM.HasClass("go-to-shop") {
+			return
+		}
+		link := h.Request.AbsoluteURL(h.Attr("href"))
+	})
+
+	c.OnHTML("a.go-to-product[href]", func(h *colly.HTMLElement) {
+		if h.DOM.HasClass("go-to-shop") {
+			return
+		}
+		link := h.Request.AbsoluteURL(h.Attr("href"))
+	})
+
+	c.Visit(url)
+	return results
+}
