91da119788996e19a1d02e8e3581cb5af5971dc3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-28 07:47:18 +0100

    format file and remove duplicate fields in struct

--- backend/search_module/search_module.go
+++ backend/search_module/search_module.go
@@ -3,14 +3,10 @@ package search_module
 import "errors"
 
 type SearchResult struct {
-   Phrase     string
-   Page       int
-   NumOfPages int
-   Results    map[string]string
-   Phrase     string
-   Page       int
-   NumOfPages int
-   Results    map[string]string
+	Phrase     string
+	Page       int
+	NumOfPages int
+	Results    map[string]string
 }
 
 type websiteType string
@@ -20,23 +16,23 @@ const (
 )
 
 type searchRequest struct {
-   phrase  string
-   page    int
-   website websiteType
+	phrase  string
+	page    int
+	website websiteType
 }
 
 type WebsiteSearch interface {
-   GetResults(phrase string, page int) (SearchResult, error)
+	GetResults(phrase string, page int) (SearchResult, error)
 }
 
 type searchModule struct {
-   websites []WebsiteSearch
+	websites []WebsiteSearch
 }
 
 func New(websites []WebsiteSearch) (searchModule, error) {
-   if len(websites) == 0 {
-       return searchModule{}, errors.New("Search module should have at least one website")
-   }
-   search := searchModule{websites: websites}
-   return search, nil
+	if len(websites) == 0 {
+		return searchModule{}, errors.New("Search module should have at least one website")
+	}
+	search := searchModule{websites: websites}
+	return search, nil
 }
