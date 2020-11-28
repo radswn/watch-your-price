436b570c2d45a99481c9df9170f4f275bee501ac
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-28 07:47:56 +0100

    create base file for testing search module

---
+++ backend/search_module/search_module_test.go
@@ -0,0 +1,25 @@
+package search_module_test
+
+import (
+	"backend/search_module"
+	"fmt"
+	"testing"
+)
+
+type testWebsiteSearch struct {
+}
+
+func (tws testWebsiteSearch) GetResults(phrase string, page int) (search_module.SearchResult, error) {
+	sr := search_module.SearchResult{
+		Phrase:     phrase,
+		Page:       page,
+		NumOfPages: 5,
+		Results: map[string]string{
+			"result1": "example.com/1",
+			"result2": "example.com/2",
+			"result3": "example.com/3",
+			"result4": "example.com/4",
+		},
+	}
+	return sr, nil
+}
