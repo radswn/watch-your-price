ff404812dbaeec2cef664021dc64251404e9d8a2
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-28 14:37:42 +0100

    add function for creating search module object
    It is using testing implementation for WebsiteSearch

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"backend/search_module"
+	"backend/search_module/website_type"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 var db = make(map[string]string)
+var searchModule = setupSearchModule()
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
@@ -56,6 +59,35 @@ func setupRouter() *gin.Engine {
 	})
 
 	return r
+}
+
+// use this implementation of website search before I port ceneo module - Dawid
+type testWebsiteSearch struct {
+}
+
+func (tws testWebsiteSearch) GetResults(phrase string, page int) (search_module.SearchResult, error) {
+	sr := search_module.SearchResult{
+		Phrase:     phrase,
+		Page:       page,
+		NumOfPages: 5,
+		Results: map[string]string{
+			"result1": "example.com/1",
+			"result2": "example.com/2",
+			"result3": "example.com/3",
+			"result4": "example.com/4",
+		},
+	}
+	return sr, nil
+}
+
+func setupSearchModule() *search_module.SearchModule {
+	searchModule, err := search_module.New(map[website_type.WebsiteType]search_module.WebsiteSearch{
+		website_type.Ceneo: testWebsiteSearch{},
+	})
+	if err != nil {
+		panic("Can't initilize search module")
+	}
+	return searchModule
 }
 
 func main() {
