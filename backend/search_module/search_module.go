5c8b99499188dc132f25e64f0a947a10cd3e5f08
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-30 08:21:59 +0100

    add comments to search module

--- backend/search_module/search_module.go
+++ backend/search_module/search_module.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 )
 
+// SearchResult represent the response from the website search
 type SearchResult struct {
 	Phrase     string            `json:"phrase" binding:"required"`
 	Page       int               `json:"page" binding:"required"`
@@ -12,20 +13,24 @@ type SearchResult struct {
 	Results    map[string]string `json:"results" binding:"required"`
 }
 
+// SearchRequest represent query to the specific website search
 type SearchRequest struct {
 	Phrase  string                   `json:"phrase" binding:"required"`
 	Page    int                      `json:"page" binding:"required"`
 	Website website_type.WebsiteType `json:"website" binding:"required"`
 }
 
+// WebsiteSearch defines interface that has to be implemented by any website search
 type WebsiteSearch interface {
 	GetResults(phrase string, page int) (SearchResult, error)
 }
 
+// SearchModule represent struct used to execute methods related to searching
 type SearchModule struct {
 	websites map[website_type.WebsiteType]WebsiteSearch
 }
 
+// New returns new instance of SearchModule with provided websites
 func New(websites map[website_type.WebsiteType]WebsiteSearch) (*SearchModule, error) {
 	if len(websites) == 0 {
 		return nil, errors.New("Search module should have at least one website")
@@ -34,6 +39,7 @@ func New(websites map[website_type.WebsiteType]WebsiteSearch) (*SearchModule, er
 	return search, nil
 }
 
+// Search takes SearchRequest parameter, performs query to specific website data and returns results
 func (sm SearchModule) Search(request SearchRequest) (*SearchResult, error) {
 	result, err := sm.websites[request.Website].GetResults(request.Phrase, request.Page)
 	if err != nil {
