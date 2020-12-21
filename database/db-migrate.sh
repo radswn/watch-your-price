d61c8d1df02f2b5f55df72fef13af9d6770a5abe
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-21 16:04:31 +0100

    add goto command to script

--- database/db-migrate.sh
+++ database/db-migrate.sh
@@ -4,11 +4,16 @@ usage() {
     echo "db-migrate.sh <COMMAND>"
     echo ""
     echo "COMMANDS:"
-    echo "create <name> - create new migration script with given name"
+    echo "create <name> - create new migration script with given <name>"
+    echo "goto <ver> - migrate the database to version <ver>"
 }
 
 create_migration () {
     docker run -it --rm -v "$PWD"/migrations:/migrations migrate/migrate create -ext sql -dir /migrations -seq "$1"
+}
+
+goto_version () {
+    docker run -it --rm -v "$PWD"/migrations:/migrations --network host migrate/migrate -path=/migrations/ -database 'mysql://sa:!QAZxsw2@tcp(localhost:3306)/mydb' goto "$1"
 }
 
 case "$1" in
@@ -19,6 +24,14 @@ case "$1" in
           usage
         fi
         ;;
+    goto)
+        if [ -n "$2" ]; then
+          goto_version "$2"
+        else
+          usage
+        fi
+        ;;
+
     *)
         usage
         ;;
