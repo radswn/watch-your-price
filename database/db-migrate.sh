8ab740c9d7cf04b9caff6aa30c7169a7e6eed5ba
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-21 15:50:40 +0100

    add create command to migration script

--- database/db-migrate.sh
+++ database/db-migrate.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+usage() {
+    echo "db-migrate.sh <COMMAND>"
+    echo ""
+    echo "COMMANDS:"
+    echo "create <name> - create new migration script with given name"
+}
 
+create_migration () {
+    docker run -it --rm -v "$PWD"/migrations:/migrations migrate/migrate create -ext sql -dir /migrations -seq "$1"
+}
+
+case "$1" in
+    create)
+        if [ -n "$2" ]; then
+          create_migration "$2"
+        else
+          usage
+        fi
+        ;;
+    *)
+        usage
+        ;;
+esac
