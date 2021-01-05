a4e7875f2ec905572cd4f6bb7f86d7ce8bf374a4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-01-05 08:48:22 +0100

    create ceneo search struct

--- backend/search_module/websites/ceneo.go
+++ backend/search_module/websites/ceneo.go
@@ -1,1 +1,10 @@
 package websites
+
+type ceneoSearch struct {
+}
+
+// New returns new instance of SearchModule with provided websites
+func New() (*ceneoSearch, error) {
+	search := &ceneoSearch{}
+	return search, nil
+}
