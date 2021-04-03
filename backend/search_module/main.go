7328e3b0cd954a53be355e60afbb94e60ae5b320
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-03 21:10:03 +0200

    setup logrus and search module in main

--- backend/search_module/main.go
+++ backend/search_module/main.go
@@ -1,1 +1,54 @@
 package search_module
+
+import (
+	"backend/search_module/website_type"
+	"backend/search_module/websites"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"runtime"
+	"strings"
+)
+
+var searchModule *SearchModule
+
+func init() {
+	setupLogrus()
+	searchModule = setupSearchModule()
+}
+
+func main() {
+
+}
+
+func setupLogrus() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		CallerPrettyfier: func(r *runtime.Frame) (function string, file string) {
+			filepath := strings.Split(r.File, "/")
+			return "", fmt.Sprintf("%s:%v", filepath[len(filepath)-1], r.Line)
+		},
+	})
+
+	file, err := os.OpenFile("search.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		logrus.SetOutput(os.Stdout)
+		logrus.WithError(err).Warn("Cannot open log file. Logging to stdout.")
+	} else {
+		logrus.SetOutput(file)
+	}
+
+	// adds information about location of log
+	logrus.SetReportCaller(true)
+}
+
+func setupSearchModule() *SearchModule {
+	ceneoSearch := websites.New(website_type.Ceneo)
+	searchModule, err := New(map[website_type.WebsiteType]WebsiteSearch{
+		website_type.Ceneo: ceneoSearch,
+	})
+	if err != nil {
+		logrus.WithError(err).Panic("Can't initialize search module.")
+	}
+	return searchModule
+}
