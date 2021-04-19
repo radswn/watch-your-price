077a32b336abf0964eb11e34f19a39057ca0e08f
Aleksander Malcew <amalcew@protonmail.com>
2021-04-19 19:16:59 +0200

    added link field to product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-class Products(models.Model):
+class Product(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     name = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
     price = models.IntegerField()  # do przedyskutowania
 
     def __str__(self):
