eccb8594313bfbf6a9a2c67ac3244dff82af9cba
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-19 23:09:31 +0200

    add check request and result

--- backend/search_module/scraper/scraper_check.go
+++ backend/search_module/scraper/scraper_check.go
@@ -1,1 +1,12 @@
 package scraper
+
+// CheckResult represent the response from the website price check
+type CheckResult struct {
+	Price string `json:"price" binding:"required"`
+}
+
+// CheckRequest represent query to the specific website price check
+type CheckRequest struct {
+	Url     string      `json:"phrase" binding:"required"`
+	Website WebsiteType `json:"website" binding:"required"`
+}
